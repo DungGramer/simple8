b28d1d89c228971cd4688695a859de1235a88c7f
DungGramer <dung.dev.gramer@gmail.com>
2020-05-20 02:27:02 +0700

    change theme Bootstrap

--- index.php
+++ index.php
@@ -1,138 +1,95 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+	<head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Simple8</title>
-	<!-- Font Awesome -->
-	<link rel="stylesheet" href="./styles/fontawesome/css/all.min.css">
-	<link rel="stylesheet" href="./styles/fontawesome/css/fontawesome.min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-
-	<!-- Google Fonts -->
-	<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400&display=swap" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
+	<title>Document</title>
+		<link rel="stylesheet" href="./styles/css/base.css">
+		<link rel="stylesheet" href="./styles/css/index.css">
 	
-	<link rel="stylesheet" href="./styles/css/index.css">
-</head>
-<body>
-<?php require_once('./module/header.php'); ?>
+		<!-- Font Awesome -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<!--Scroll reveal CDN-->
+		<script defer src="https://unpkg.com/scrollreveal"></script>
+	</head>
+	<body>
+		<?php require_once('module/header.php'); ?>
+		<!-- Header end -->
+		<section class="main" id="main">
+			<div id="carousel" class="carousel slide" data-ride="carousel">
+				<div class="carousel-inner">
 
-	<main>
-		<div class="collection">
-			<img src="./img/files/collection1.jpg" alt="collection1" class="collection__img collection__img--1">
+					<div class="carousel-item active">
+						<img class="d-block img-fluid " src="img/files/StrateWindow-Bis-Desk.webp" alt="">
+						<div class="carousel-caption-men carousel-caption d-none d-sm-block">
+							<h5 class="carousel__title--men text-dark">Đồ nam</h5>
+							<button type="button" class="carousel__button btn btn-dark">Khám phá</button>
+						</div>
+					</div>
+
+					<div class="carousel-item">
+						<img class="d-block img-fluid " src="img/files/2.jpg" alt="">
+						<div class="carousel-caption d-none d-sm-block">
+							<h5>Đồ nam</h5>
+							<small>
+								Khám phá
+							</small>
+						</div>
+					</div>
+
+					<div class="carousel-item">
+						<img class="d-block img-fluid " src="img/files/3.jpg" alt="">
+						<div class="carousel-caption d-none d-sm-block">
+							<h5>Đồ nam</h5>
+							<small>
+								Khám phá
+							</small>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</section>
+		<!-- Main end -->
+		
+		<section class="top-product">
+			<div class="container-fluid">
+				<div class="row d-flex justify-content-around align-items-center">
+					<div class="col-md-3">
+						<img src="img/files/men.jpg" alt="">
+						<h3>Đồ nam</h3>
+					</div>
+
+					<div class="col-md-3">
+						<img src="img/files/women.jpg" alt="">
+						<h3>Đồ nam</h3>
+					</div>
+
+					<div class="col-md-3">
+						<img src="img/files/shoes.jpg" alt="">
+						<h3>Đồ nam</h3>
+					</div>
+				</div>
+			</div>
+		</section>
+	<!-- Story end -->
+
+	<!-- Carousel end -->
+	<section class="choose-3">
+		<h2 class="sub-headline">Lựa chọn</h2>
+		<h1 class="headline headline-dark">kiểu dáng</h1>
+		<div class="container">
+			<div class="div3 man" style="background-image: url('./img/files/men.jpg');" onClick="window.location='./product.php?type=sm';"></div>
+			<div class="div3 women" style="background-image: url('./img/files/women.jpg');"onClick="window.location='./product.php';"></div>
+			<div class="div3 shoes" style="background-image: url('./img/files/shoes.jpg');"onClick="window.location='./product.php';"></div>
 		</div>
-
-		<div class="product">
-			<div class="card">
-				<div class="card__header">
-						<h1 class="card__header--title">Phụ kiện</h1>
-						<a href="" class="card__header--more">Xem tất cả</a>
-				</div>
-				<div class="card__image">
-					<img src="./img/collections/phukien.jpg" alt="phukien" class="card__image--img">
-				</div>
-			</div>
-
-			<div class="card">
-				<div class="card__header">
-					<h1 class="card__header--title">Áo thun</h1>
-					<a href="" class="card__header--more">Xem tất cả</a>
-				</div>
-				<div class="card__image">
-					<img src="./img/collections/aothun.jpg" alt="phukien" class="card__image--img">
-				</div>
-			</div>
-
-			<div class="card">
-				<div class="card__header">
-					<h1 class="card__header--title">Áo khoác</h1>
-					<a href="" class="card__header--more">Xem tất cả</a>
-				</div>
-				<div class="card__image">
-					<img src="./img/collections/aokhoac.jpg" alt="phukien" class="card__image--img">
-				</div>
-			</div>
-
-			<div class="card">
-				<div class="card__header">
-					<h1 class="card__header--title">Áo sơ mi</h1>
-					<a href="" class="card__header--more">Xem tất cả</a>
-				</div>
-				<div class="card__image">
-					<img src="./img/collections/somi.jpg" alt="phukien" class="card__image--img">
-				</div>
-			</div>
-
-			<div class="card">
-				<div class="card__header">
-					<h1 class="card__header--title">Áo len</h1>
-					<a href="" class="card__header--more">Xem tất cả</a>
-				</div>
-				<div class="card__image">
-					<img src="./img/collections/aolen.jpg" alt="phukien" class="card__image--img">
-				</div>
-			</div>
-
-			<div class="card">
-				<div class="card__header">
-					<h1 class="card__header--title">Hoddie</h1>
-					<a href="" class="card__header--more">Xem tất cả</a>
-				</div>
-				<div class="card__image">
-					<img src="./img/collections/hoddie.jpg" alt="phukien" class="card__image--img">
-				</div>
-			</div>
-		</div>
-		<!-- Áo thun -->
-		<div class="tshirt-collection">
-				<div class="tshirt tshirt__header">
-					<a href="">
-						<img src="./img/collections/tshirt-header.jpg" alt="tshirt-header" class="tshirt__header--img">
-					</a>
-				</div>
-
-				<div class="tshirt tshirt__item">
-					<img src="./img/products/tshirt_xanhhaiquan (1).jpg" alt="xanhhaiquan" class="tshirt__item--img">
-					<div class="tshirt__content">
-					<span class="tshirt__item--header"><br />xanh hải quân</span>
-					<span class="tshirt__item--cost"><br />1.600.000 đ</span>
-					</div>
-				</div>
-
-				<div class="tshirt tshirt__item">
-					<img src="./img/products/tshirt_do (1).jpg" alt="xanhhaiquan" class="tshirt__item--img">
-					<div class="tshirt__content">
-					<span class="tshirt__item--header"><br />T-shirt đỏ</span>
-					<span class="tshirt__item--cost"><br />1.600.000 đ</span>
-					</div>
-				</div>
-				
-				<div class="tshirt tshirt__item">
-					<img src="./img/products/tshirt_den (5).jpg" alt="xanhhaiquan" class="tshirt__item--img">
-					<div class="tshirt__content">
-					<span class="tshirt__item--header"><br />T-shirt đen</span>
-					<span class="tshirt__item--cost"><br />1.600.000 đ</span>
-					</div>
-				</div>
-
-				<div class="tshirt tshirt__item">
-					<img src="./img/products/tshirt_xanhbien (5).jpg" alt="xanhhaiquan" class="tshirt__item--img">
-					<div class="tshirt__content">
-					<span class="tshirt__item--header"><br />T-shirt xanh biển</span>
-					<span class="tshirt__item--cost"><br />1.600.000 đ</span>
-					</div>
-				</div>
-				
-				<div class="tshirt tshirt__item">
-					<img src="./img/products/tshirt_xam (5).jpg" alt="xanhhaiquan" class="tshirt__item--img">
-					<div class="tshirt__content">
-					<span class="tshirt__item--header"><br />T-shirt xám</span>
-					<span class="tshirt__item--cost"><br />1.600.000 đ</span>
-					</div>
-				</div>
-		</div>
-	<?php require_once('./module/footer.php') ?>
-	</main>
+	</section>
+	<!-- Choose 3 end -->
+	<?php require_once('module/footer.php'); ?>
+	<script src="./script/index.js"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
--- module/header.php
+++ module/header.php
@@ -1,7 +1,9 @@
 <link rel="stylesheet" href="./styles/css/header.css">
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css2?family=Megrim&display=swap" rel="stylesheet">
 <header>
-	<div class="container">
+<!-- 	<div class="container">
 		<nav class="nav">
 			<div class="menu-toggle">
 				<i class="fas fa-bars"></i>
@@ -29,6 +31,40 @@
 				</li>
 			</ul>
 		</nav>
-	</div>
-	<script src="./script/header.js"></script>
+	</div> -->
+
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<div class="header__logo d-flex justify-content-between">
+			<a class="navbar-brand" href="#">
+				<img src="img/files/logo-black.svg" width="100" hieght="30" alt="logo">
+			</a>
+		<button class="header__button navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		</div>
+		<div class="header__content collapse navbar-collapse justify-content-between" id="navbarNav">
+			<div class="content">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="header__nav-link nav-link" href="#">Đồ Nam <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="header__nav-link nav-link" href="#">Đồ Nữ</a>
+					</li>
+					<li class="nav-item">
+						<a class="header__nav-link nav-link" href="#">Giày</a>
+					</li>
+			</div>
+				<div class="cart" >
+					<i class="header__nav-link fal fa-shopping-cart"></i>
+					<span class="cart__quantity">1</span>
+				</div>
+			</ul>
+		</div>
+</nav>
 </header>
+<!-- <script defer src="script/header.js"></script> -->
+<script defer src="script/all.min.js"></script>
+<script defer src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script defer src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
